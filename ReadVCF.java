50430c1275a00e9b621b43ca3c1aa098d086d8f3
Anthony Meehan <acm49@students.waikato.ac.nz>
2017-09-07 14:01:07 +1200

    Commented out exon stuff, extracted genotype

--- ReadVCF.java
+++ ReadVCF.java
@@ -20,7 +20,7 @@ class ReadVCF {
     	
     	//VCFData/wgEncodeGencodeBasicV17.txt"
     	
-    	ArrayList<ExonRegion> exonRegions = ExonParser.getExonRegions(args[1]);
+    	//ArrayList<ExonRegion> exonRegions = ExonParser.getExonRegions(args[1]);
     	
     	
         try {
@@ -28,7 +28,7 @@ class ReadVCF {
 		    String theLine = theScanner.nextLine();
 		    String[] splitLines;
 		    //Ignore metadata
-		    while (theLine.matches("^##")) {
+		    while (theLine.matches("^##.*")) {
 		    	theLine = theScanner.nextLine();
 		    }
 		    //Remove #
@@ -38,17 +38,35 @@ class ReadVCF {
 		    
 		    int chromIndex = getHeaderIndex("CHROM", header);
 		    int posIndex = getHeaderIndex("POS", header);
+		    int fatIndex = getHeaderIndex("FATHER", header);
+		    int son2Index = getHeaderIndex("SON2", header);
 		    
 		    //The Records
 		    while (theScanner.hasNext()) {
 		        theLine = theScanner.nextLine();
 		        splitLines = theLine.split("\\s+");
 		        
-		        String chromosome = "chr"+splitLines[chromIndex];
-		        int position = Integer.parseInt(splitLines[posIndex]);
+		        //String chromosome = "chr"+splitLines[chromIndex];
+		        //int position = Integer.parseInt(splitLines[posIndex]);
+		        
+		        String[] familyMembers = new String[(son2Index-fatIndex)+1];
+		        //System.out.println(son2Index-fatIndex);
+		        
+		        for (int i=fatIndex; i<=son2Index; i++) {
+		        	familyMembers[i-fatIndex] = splitLines[i];
+		        }
 		        
 		        //System.out.println(chromosome + " , " + position);
 		        
+		        for (String familyMember : familyMembers) {
+		        	System.out.print(familyMember.split(":")[0] + "\t");
+		        }
+		        System.out.println();
+		        
+		        
+		        
+		        
+		        /*
 		        for (ExonRegion theExonRegion : exonRegions) {
 		        	if (theExonRegion.chromosome.equals(chromosome)) {
 				    	for (Range theRange : theExonRegion.ranges) {
@@ -59,7 +77,7 @@ class ReadVCF {
 			    			}
 				    	}
 		        	}
-		        }
+		        }*/
 		        
 		    }
         }
