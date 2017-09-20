6a21f11dcdccba6eabb09f7149e7aa3a8bb3fc01
caw40 <caw40@cms-r6-71.cms.waikato.ac.nz>
2017-09-20 13:11:41 +1200

    Cleaned up code

--- RecordParser.java
+++ RecordParser.java
@@ -3,38 +3,13 @@ import java.util.*;
 
 public class RecordParser {
     public static void main(String[] args) throws Exception {
-        //ArrayList<Record> recs = new ArrayList<Record>();
-        //args[0] is ALL.merged
-        //args[1] is filtered
-        //boolean process = false;
-        //Scanner recordScanner = getVCFScanner("/home/tcs/public_html/COMP555/VCFdata/ALL.merged.phase1_release_v3.20101123.snps_indels_svs.vcf");
-        VCFScanner knownVariantsVCFScanner = new VCFScanner("../VCFdata/ALL.merged.phase1_release_v3.20101123.snps_indels_svs.vcf", false);
-        VCFScanner queryVariantVCFScanner = new VCFScanner("filteredResults.vcf", false);
-        //Scanner recordScanner = getVCFScanner("../filteredResults2.txt");
-        //Scanner resultScanner = getVCFScanner("../filteredResults.txt");
-
-        /*
-        ArrayList<Integer[]> present = new ArrayList<Integer[]>();
-        while (sc2.hasNextLine()) {
-            String line2 = sc2.nextLine();
-            if (line2.contains("#")) {
-                continue;
-            }
-            String[] splits2 = line2.split("\\s+");
-
-            Integer[] temp = new Integer[2];
-            temp[0] = Integer.parseInt(splits2[1]);
-            temp[1] = Integer.parseInt(splits2[2]);
-
-            present.add(temp);
-        }*/
-
-        /*
-        String line = recordScanner.nextLine();
-        //Skip long header
-        while (line.contains("#")) {
-            line = recordScanner.nextLine();
-        }*/
+    	
+    	String KNOWN_VARIANTS_FILE = "/home/tcs/public_html/COMP555/VCFdata/ALL.merged.phase1_release_v3.20101123.snps_indels_svs.vcf";
+    	String QUERY_VARIANTS_FILE = "filteredResults2.vcf";
+    
+       	VCFScanner knownVariantsVCFScanner = new VCFScanner(KNOWN_VARIANTS_FILE, false);
+        VCFScanner queryVariantVCFScanner = new VCFScanner(QUERY_VARIANTS_FILE, false);
+        
 
         String knownVariantLine;
 
@@ -61,13 +36,13 @@ public class RecordParser {
             //Known variants file is not numerically ordered: Reset queryVariant scanner after 19->2, 22->3 transitions
             if ((chr == 2 && !seen2)) {
                 seen2 = true;
-                System.out.println("\nRESETTING FROM 2\n");
-                queryVariantVCFScanner = new VCFScanner("../filteredResults.txt", false);
+                System.err.println("\nRESETTING FROM 2\n");
+                queryVariantVCFScanner = new VCFScanner(QUERY_VARIANTS_FILE, false);
                 resultChrPos = getNextChrPos(queryVariantVCFScanner.scanner, resultChrPos);
             } else if (chr == 3 && !seen3) {
                 seen3 = true;
-                System.out.println("\nRESETTING FROM 3\n");
-                queryVariantVCFScanner = new VCFScanner("../filteredResults.txt", false);
+                System.err.println("\nRESETTING FROM 3\n");
+                queryVariantVCFScanner = new VCFScanner(QUERY_VARIANTS_FILE, false);
                 resultChrPos = getNextChrPos(queryVariantVCFScanner.scanner, resultChrPos);
             }
 
@@ -83,45 +58,16 @@ public class RecordParser {
             //System.out.println(lineCount + " ALL.merged.Record: chr=" + chr + " pos=" + pos + " <=====>" + " filteredResults.txt: chr=" + resultChrPos[0] + " pos=" + resultChrPos[1]);
 
             if (resultChrPos[0] == chr && resultChrPos[1] == pos) {
-                System.out.println("\n" + knownVariantLine + "\n");
+                System.out.println(knownVariantLine);
             }
 
-            if ((lineCount++ % 100000) == 1) {
-                System.out.println(lineCount + " ALL.merged.Record: chr=" + chr + " pos=" + pos + " <=====>" + " filteredResults.txt: chr=" + resultChrPos[0] + " pos=" + resultChrPos[1]);
+            if ((lineCount++ % 100000) == 100000-1) {
+                System.err.println(lineCount + " ALL.merged.Record: chr=" + chr + " pos=" + pos + " <=====>" + " filteredResults.txt: chr=" + resultChrPos[0] + " pos=" + resultChrPos[1]);
             }
         }
 
-        /*
-        for(Integer[] in : present) {
-            if (in[0].equals(c1) && in[1].equals(pos1)) {
-                System.out.println(line);
-            }
-        }*/
     }
       
-      
-      /*if(process) {
-        String[] acan = splits[7].split(";");
-        int ac = 0;
-        int an = 0;
-        for(int i = 0; i < 2; i++) {
-          if(acan[0].contains("AC")) {
-            ac = Integer.parseInt(acan[0].split("=")[1]);
-            an = Integer.parseInt(acan[1].split("=")[1]);
-          } else {
-            ac = Integer.parseInt(acan[1].split("=")[1]);
-            an = Integer.parseInt(acan[0].split("=")[1]);
-          }
-        }
-        recs.add(new Record(Integer.parseInt(splits[0]), Integer.parseInt(splits[1]),
-          splits[2], splits[3], splits[4], splits[5], splits[6], ac, an));
-      }
-      if(splits[0].contains("#CHROM")) {
-        process = true;
-      }
-    }
-    for(int i = 0; i < recs.size(); i++)
-      System.out.println(recs.get(i).prettyString());*/
 
     private static int[] getNextChrPos(Scanner resultScanner, int[] resultChrPos) {
         String[] splits2 = resultScanner.nextLine().split("\\s+");
